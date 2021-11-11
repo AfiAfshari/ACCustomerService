485ace90387dd4c7792cd8b2e342c197547097a8
Afshinog <afshina078@gmail.com>
2021-11-11 10:58:32 -0500

    init

---
+++ ACCusotmerService/Controllers/ACCustomerServiceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ACCusotmerService.ViewModel;
+using BAL;
+using EntityModel;
+
+namespace ACCusotmerService.Controllers
+{
+    public class ACCustomerServiceController : Controller
+    {
+        public ActionResult ACCustomerService()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetACCustomerService()
+        {
+            try
+            {
+                var _objBAL = new BAL.ACCustomerServiceBAL();
+                List<ACCustomerServiceEntity> lstEntity = new List<ACCustomerServiceEntity>();
+                lstEntity = _objBAL.GetACCustomerService();
+
+                ACCustomerServiceViewModel viewModel = null;
+                List<ACCustomerServiceViewModel> lstViewModel = new List<ACCustomerServiceViewModel>();
+
+                if (lstEntity != null && lstEntity.Count > 0)
+                {
+                    foreach (var item in lstEntity)
+                    {
+                        viewModel = new ACCustomerServiceViewModel();
+
+                        viewModel.CustomerID = item.CustomerID;
+                        viewModel.FirstName = item.FirstName;
+                        viewModel.LastName = item.LastName;
+                        viewModel.Address = item.Address;
+                        viewModel.PhoneNumber = item.PhoneNumber;
+                        viewModel.DateCalled = item.DateCalled;
+                        viewModel.DateCreated = item.DateCreated;
+
+                        lstViewModel.Add(viewModel);
+                    }
+                }
+
+                return Json(lstViewModel, JsonRequestBehavior.AllowGet);
+            }
+            catch(Exception exc)
+            {
+                throw exc;
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/ACCustomerServiceDAL.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using EntityModel; 
+
+namespace DataAccessLayer
+{
+    public class ACCustomerServiceDAL
+    {
+        const string connectionString = @"Server=DESKTOP-JVB24ET\SQLEXPRESS01;Database=ACCustomerService;Trusted_Connection=True;";
+        SqlConnection sqlcon;
+        SqlParameter param;
+        SqlDataReader dr;
+
+        public List<ACCustomerServiceEntity> GetACCustomerService()
+        {
+            ACCustomerServiceEntity entity = null;
+            List<ACCustomerServiceEntity> lstEntity = new List<ACCustomerServiceEntity>();
+
+            try
+            {
+                using (sqlcon = new SqlConnection(connectionString))
+                {
+                    DataSet ds = new DataSet();
+                    SqlCommand cmd = new SqlCommand("dbo.GetACCustomers", sqlcon);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    sqlcon.Open();
+
+                    if (ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
+                    {
+                        foreach (DataRow dr in ds.Tables[0].Rows)
+                        {
+                            entity = new ACCustomerServiceEntity();
+
+                            entity.CustomerID = Convert.ToInt32(dr["CustomerID"].ToString());
+                            entity.FirstName = string.IsNullOrEmpty(dr["FirstName"].ToString()) ? "" : (dr["FirstName"].ToString());
+                            entity.LastName = string.IsNullOrEmpty(dr["LastName"].ToString()) ? "" : (dr["FirstName"].ToString());
+                            entity.Address = string.IsNullOrEmpty(dr["Address"].ToString()) ? "" : (dr["Address"].ToString());
+                            entity.PhoneNumber = string.IsNullOrEmpty(dr["PhoneNumber"].ToString()) ? "" : (dr["PhoneNumber"].ToString());
+                            entity.DateCalled = string.IsNullOrEmpty(dr["DateCalled"].ToString()) ? (DateTime?)null : Convert.ToDateTime(dr["DateCalled"].ToString());
+                            entity.DateCreated = string.IsNullOrEmpty(dr["DateCreated"].ToString()) ? (DateTime?)null : Convert.ToDateTime(dr["DateCreated"].ToString());
+
+                            lstEntity.Add(entity);
+                        }
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.Message);
+            }
+            finally
+            {
+                if (dr != null)
+                {
+                    dr.Close();
+                    sqlcon.Close();
+                }
+            }
+
+            return lstEntity;
+        }
+    }
+}

ef72c0205cf467ea43627e9b75c479b5d361b39c
khashayar <rkhashayar@live.com>
2021-11-15 09:10:26 -0600

    add renamin and some clean up

--- ACCusotmerService/App_Start/FilterConfig.cs
+++ ACCusotmerService/App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace ACCusotmerService
+namespace AcCustomerService
 {
     public class FilterConfig
     {
--- ACCusotmerService/ViewModel/ACCustomerServiceViewModel.cs
+++ ACCusotmerService/ViewModel/ACCustomerServiceViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ACCusotmerService.ViewModel
+namespace AcCustomerService.ViewModel
 {
     public class ACCustomerServiceViewModel
     {

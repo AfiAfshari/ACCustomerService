2d5efeadde6f2ca5b4bd7021a2c7e899aae78626
Afshinog <afshina078@gmail.com>
2022-01-13 17:47:15 -0500

    ScalarFunction

--- SQL files/CreateFunctions.sql
+++ SQL files/CreateFunctions.sql
@@ -15,3 +15,45 @@ GO
 
 SELECT * FROM [dbo].[UpperFirstNames] ()
 Go
+
+
+
+CREATE FUNCTION dbo.ScalarFunction (
+	@FirstName Varchar(Max),
+	@LastName VarChar(Max)
+)
+RETURNS VarChar(Max) AS
+BEGIN
+    
+    RETURN Concat(@FirstName , ' ' , @LastName)
+END;
+
+
+
+
+USE [ACCustomerServiceV2]
+GO
+
+
+DROP FUNCTION [dbo].[ScalarFunction]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[ScalarFunction] (
+	@FirstName Varchar(Max),
+	@LastName VarChar(Max)
+)
+RETURNS VarChar(Max) AS
+BEGIN
+    
+    RETURN Concat(@FirstName , ' ' , @LastName)
+END;
+
+
+GO
